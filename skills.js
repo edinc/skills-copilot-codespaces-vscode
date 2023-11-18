d4bdb7e4f75425d5bf8c81b4d9274a49bf3c1a60
Edin Cenanovic <cenanovice@gmail.com>
2023-11-18 16:36:07 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,12 @@
+function calculateNumbers(var1, var2)
+{
+    var sum = var1 + var2;
+    var diff = var1 - var2;
+    var product = var1 * var2;
+    var quotient = var1 / var2;
+
+    console.log("Sum: " + sum);
+    console.log("Difference: " + diff);
+    console.log("Product: " + product);
+    console.log("Quotient: " + quotient);
+}
